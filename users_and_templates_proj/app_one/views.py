11031ed875319179c69115dc1995bda35984b613
JasonVolkoff <kashijason@gmail.com>
2021-05-13 03:06:29 -0700

    linked paths from views to urls; added django context names in html forms; updated models to include User class

--- users_and_templates_proj/app_one/views.py
+++ users_and_templates_proj/app_one/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, redirect
+from .models import User
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        "all_users": User.objects.all()
+    }
+    return render(request, 'index.html', context)
 
 
 def add_user(request):
+    User.objects.create(
+        first_name=request.POST['first_name'],
+        last_name=request.POST['last_name'],
+        email=request.POST['email'],
+        age=request.POST['age']
+    )
     return redirect('/')
